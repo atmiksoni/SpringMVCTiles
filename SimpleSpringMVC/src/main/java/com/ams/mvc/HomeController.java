8286966fe68134db4e9bbebe7f5ae8bbb60d5e43
soniiatm <atmik.soni@tieto.com>
2017-01-12 12:07:54 +0530

    Just changing label

--- SimpleSpringMVC/src/main/java/com/ams/mvc/HomeController.java
+++ SimpleSpringMVC/src/main/java/com/ams/mvc/HomeController.java
@@ -1,15 +1,15 @@
 package com.ams.mvc;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	 @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
 	    public String home(Locale locale, Model model) {
-	        logger.info("Welcome home! Thejj client locale is  dfd {}.", locale);
+	        logger.info("Welcome home! The client locale is  Location {}.", locale);
 	         
 	        Date date = new Date();
 	        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
